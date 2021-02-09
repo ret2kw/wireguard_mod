e9ec080bf03829e65d25aae667d659488c7290ec
Jessey Bullock <jessey.bullock@gmail.com>
2021-02-09 16:55:06 -0600

    making it work again, docker-for-desktop got rid of the sr0 device for their linuxkit build

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# https://github.com/linuxkit/linuxkit/issues/3402
-# homeboy made a docker image that can read out the kernel image name from the cd docker-desktop boots from
-DOCKER_VER=$(docker run --rm --device=/dev/sr0 quay.io/steigr/docker-for-desktop-get-kernel-image:latest)
-docker build --build-arg DOCKER_VER=${DOCKER_VER} -t wg_mod .
+# pulling the kernel image from /etc/linuxkit.yml on the host with a complete disregard for all decency be just downloading an entire python project and jq just to parse the yaml on the command line
+DOCKER_VER=$(docker run --rm --privileged --pid=host alpine sh -c 'apk add py-pip > /dev/null 2>&1 && apk add jq > /dev/null 2>&1 && pip install yq > /dev/null 2>&1 && nsenter -m -t 1 cat /etc/linuxkit.yml | yq -r .kernel.image')
+
+# terrible way of pulling the particular kernel version, could probably just uname during build but meh
+KERNEL_VER=$(echo $DOCKER_VER | cut -f 2 -d : | cut -f 1 -d -)
+
+docker build --no-cache --build-arg DOCKER_VER=${DOCKER_VER} --build-arg KERNEL_VER=${KERNEL_VER} -t wg_mod .
 
 #now we just run the container to load the module, this needs to happen after every reboot
 docker run --rm --privileged wg_mod
